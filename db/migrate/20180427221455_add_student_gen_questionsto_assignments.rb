b712a129ffdf53a66969d99f1b315b783f7341fa
expertiza developer <praneet.mocherla@gmail.com>
2018-04-27 18:30:59 -0400

    edited last migration

--- db/migrate/20180427221455_add_student_gen_questionsto_assignments.rb
+++ db/migrate/20180427221455_add_student_gen_questionsto_assignments.rb
@@ -1,5 +1,5 @@
 class AddStudentGenQuestionstoAssignments < ActiveRecord::Migration
   def change
-    add_column :assignments, :student_gen_questions, :string, null: false, default: "false"
+    add_column :assignments, :student_gen_questions, :boolean
   end
 end
